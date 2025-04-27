8e694b73587a1091e52567c46283ed2047532c0e
hoafn0730 <hoantran0730@gmail.com>
2025-04-27 23:25:24 +0700

    refactor: remove role-based middleware from board and column routes

--- src/routes/v1/columnRouter.js
+++ src/routes/v1/columnRouter.js
@@ -6,8 +6,18 @@ import { columnValidation } from '~/validations/columnValidation';
 const router = express.Router();
 
 router.get('/:id', columnController.getOne);
-router.post('/', boardMiddleware.checkMemberRole('admin', 'owner'), columnValidation.store, columnController.store);
-router.put('/:id', boardMiddleware.checkMemberRole('admin', 'owner'), columnValidation.update, columnController.update);
+router.post(
+    '/',
+    //  boardMiddleware.checkMemberRole('admin', 'owner'),
+    columnValidation.store,
+    columnController.store,
+);
+router.put(
+    '/:id',
+    //  boardMiddleware.checkMemberRole('admin', 'owner'),
+    columnValidation.update,
+    columnController.update,
+);
 router.delete('/:id', columnValidation.destroy, columnController.destroy);
 
 export default router;
