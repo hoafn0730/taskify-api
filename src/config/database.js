a50bed37eddfcd6c281d0b7cf4f9fd9d1c0417b8
hoafn0730 <hoantran0730@gmail.com>
2025-06-02 15:15:57 +0700

    feat: update database configuration to use environment variables for improved security and flexibility

--- src/config/database.js
+++ src/config/database.js
@@ -1,24 +1,37 @@
+require('dotenv').config();
+
 module.exports = {
     development: {
-        username: 'root',
-        password: '123456',
-        database: 'taskify-db',
-        host: '127.0.0.1',
-        dialect: 'mysql',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_CONNECTION,
         logging: false,
     },
     test: {
-        username: 'root',
-        password: null,
-        database: 'database_test',
-        host: '127.0.0.1',
-        dialect: 'mysql',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_CONNECTION,
+        logging: false,
     },
     production: {
-        username: 'root',
-        password: null,
-        database: 'database_production',
-        host: '127.0.0.1',
-        dialect: 'mysql',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_CONNECTION,
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
     },
 };
