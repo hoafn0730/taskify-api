587ba8165a3c26fa3773ffa8fe6ac74157f06b30
hoafn0730 <hoantran0730@gmail.com>
2025-06-02 15:10:35 +0700

    fix: handle undefined CORS_ORIGIN environment variable to prevent errors

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,7 @@
 import { StatusCodes } from 'http-status-codes';
 import ApiError from '~/utils/ApiError';
 
-const WHITELIST_DOMAINS = process.env.CORS_ORIGIN.split(',');
+const WHITELIST_DOMAINS = (process.env.CORS_ORIGIN ?? '').split(',');
 
 export const corsOptions = {
     origin: function (origin, callback) {
